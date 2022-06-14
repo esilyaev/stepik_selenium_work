8a8b068b576ce117efb16d27b99ed80b6fce1f28
Eugeny Silyaev <esilayev@gmail.com>
2022-06-14 16:28:18 +0300

    init and setup env

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # stepik_selenium_work
+
+for install all dependencies run 
+pip install -r requirements.txt
---
+++ conftest.py
@@ -0,0 +1,39 @@
+import pytest
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+
+
+def pytest_addoption(parser):
+    parser.addoption('--browser_name', action='store', default='firefox',
+                     help="Choose browser: chrome or firefox")
+    parser.addoption('--language', action='store', default='en',
+                     help="Choose language: en, ru ... etc.")
+
+
+@pytest.fixture(scope="function")
+def browser(request):
+    browser_name = request.config.getoption("browser_name")
+    language = request.config.getoption("language")
+    browser = None
+
+    if browser_name == "chrome":
+        options = Options()
+        options.add_argument('--disable-gpu')
+        options.add_experimental_option(
+            'excludeSwitches', ['enable-logging'])
+        options.add_experimental_option(
+            'prefs', {'intl.accept_language': language})
+        print("\nstart chrome browser for test..")
+        browser = webdriver.Chrome()
+
+    elif browser_name == "firefox":
+        print("\nstart firefox browser for test..")
+        fp = webdriver.FirefoxProfile()
+        fp.set_preference("intl.accept_languages", language)
+        browser = webdriver.Firefox(firefox_profile=fp)
+
+    else:
+        raise pytest.UsageError("--browser_name should be chrome or firefox")
+    yield browser
+    print("\nquit browser..")
+    browser.quit()
