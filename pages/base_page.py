ba85f6c0733cfef9d345fb23a1acbca422046aca
Eugeny Silyaev <esilayev@gmail.com>
2022-06-14 18:36:28 +0300

    Login page object

--- pages/base_page.py
+++ pages/base_page.py
@@ -1,10 +1,21 @@
 from selenium.webdriver.remote.webdriver import WebDriver
+from selenium.webdriver.common.by import By
+from selenium.common.exceptions import NoSuchElementException
 
 
 class BasePage():
-    def __init__(self, browser: WebDriver, url: str) -> None:
+    def __init__(self, browser: WebDriver, url: str, timeout=10) -> None:
         self.browser: WebDriver = browser
         self.url = url
+        self.browser.implicitly_wait(timeout)
 
     def open(self) -> None:
         self.browser.get(self.url)
+
+    def is_element_present(self, how: By, what: str) -> bool:
+        try:
+            self.browser.find_element(how, what)
+        except NoSuchElementException:
+            return False
+
+        return True
--- test_main_page.py
+++ test_main_page.py
@@ -1,8 +1,14 @@
+from .pages.login_page import LoginPage
 from .pages.main_page import MainPage
 
+from selenium.webdriver.remote.webdriver import WebDriver
 
-def test_guest_can_go_to_login_page(browser):
+
+def test_guest_can_go_to_login_page(browser: WebDriver):
     link = "http://selenium1py.pythonanywhere.com/"
     page = MainPage(browser, link)
     page.open()
+    page.should_be_login_link()
     page.go_to_login_page()
+    login_page = LoginPage(browser, browser.current_url)
+    login_page.should_be_login_url()
